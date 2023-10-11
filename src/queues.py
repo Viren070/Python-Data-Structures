0aeb1a5cf0eba02acc69148de1af6f39a1415ebc
Viren070 <viren070@protonmail.com>
2023-10-11 20:10:01 +0100

    upload files

---
+++ src/queues.py
@@ -0,0 +1,190 @@
+"""
+Queue Classes
+
+This module contains implementations of various queue types, including a generic Queue, LinearQueue,
+IsaacsLinearQueue, and CircularQueue.
+
+Author: Viren070 on GitHub
+"""
+
+from typing import Union
+
+class Queue:
+    """
+    A generic queue class.
+
+    Attributes:
+    - type (str): The type of the queue.
+    - max_size (int): The maximum capacity of the queue.
+    - queue (list): The list representing the queue.
+    - front (int): The index pointing to the front of the queue.
+    - rear (int): The index pointing to the back of the queue.
+
+    Methods:
+    - __init__(self, max_size: int): Initializes a new Queue with the specified maximum size.
+    - display_queue(self): Displays the current status of the queue.
+    - is_full(self) -> bool: Checks if the queue is full.
+    - is_empty(self) -> bool: Checks if the queue is empty.
+    """
+    def __init__(self, max_size):
+        self.type = None
+        self.max_size = max_size
+        self.queue = [None for item in range(self.max_size)] # initialise array with size of max_size
+        self.front = 0
+        self.rear = -1
+
+    def display_queue(self):
+        '''Display the queue status'''
+        print(f"\nQueue Type: {self.type}\nQueue Array: {self.queue}\nFront: {self.front}\nRear: {self.rear}\nIs Full: {self.is_full()}\nIs Empty: {self.is_empty()}\n")
+
+    def is_full(self) -> bool:
+        ''' Return whether the queue is full or not'''
+        return self.rear + 1 == self.max_size
+
+    def is_empty(self) -> bool:
+        '''Retrun True if queue is empty and False if not'''
+        return self.front > self.rear
+
+class LinearQueue(Queue):
+    """
+    A simple implementation of a Linear Queue.
+
+    Attributes:
+    - type (str): The type of the queue, set to "Better Linear".
+    - max_size (int): The maximum capacity of the queue.
+    - queue (list): The list representing the linear queue.
+    - front (int): The index pointing to the front of the queue.
+
+    Methods:
+    - __init__(self, max_size: int): Initializes a new LinearQueue with the specified maximum size.
+    - is_empty(self) -> bool: Checks if the queue is empty.
+    - is_full(self) -> bool: Checks if the queue is full.
+    - enqueue(self, item): Adds an item to the rear of the queue.
+    - dequeue(self): Removes and returns the item from the front of the queue.
+
+    Note:
+    This implementation follows the principles of a linear queue, where elements are added at the rear
+    and removed from the front. When the queue is full, enqueue operations will be rejected.
+    Dequeue operations shift all remaining elements to the left to maintain order.
+    """
+    def  __init__(self, max_size):
+        super().__init__(max_size)
+        self.type = "Linear"
+
+    def enqueue(self, item_to_add) -> bool:
+        '''Add an item to the queue'''
+        if self.is_full():
+            return False
+        self.rear += 1
+        self.queue[self.rear] = item_to_add
+        return True
+
+    def dequeue(self):
+        '''Remove an item from the front of the queue'''
+        if self.is_empty():
+            return None
+
+        dequeued_item = self.queue[self.front]
+        self.front += 1
+        if self.front != 0:
+            for _ in range(self.front): # shift each item until the front index is 0
+                self.queue[:] = self.queue[1:] + [None] # add None to end of list after shifting each item to the left.
+            self.rear = self.rear - self.front # new rear would be the number of times shifted subtracted from old rear.
+            self.front = 0
+        return dequeued_item
+
+class IsaacsLinearQueue(LinearQueue):
+    """
+    A simplified implementation of a Linear Queue, using Isacc Computer Science examples. Read the note for why this is incorrect.
+
+    Attributes:
+    - type (str): The type of the queue, set to "Linear".
+    - max_size (int): The maximum capacity of the queue.
+    - queue (list): The list representing the linear queue.
+    - front (int): The index pointing to the front of the queue.
+    - rear (int): The index pointing to the back of the queue
+
+    Methods:
+    - __init__(self, max_size: int): Initializes a new LinearQueue with the specified maximum size.
+    - is_empty(self) -> bool: Checks if the queue is empty.
+    - is_full(self) -> bool: Checks if the queue is full.
+    - enqueue(self, item): Adds an item to the rear of the queue.
+    - dequeue(self): Removes and returns the item from the front of the queue.
+
+    Note:
+    This implementation follows **MOST** principles of a linear queue, where elements are added at the rear
+    and removed from the front. When the queue is full, enqueue operations will be rejected.
+    Dequeue operations **DO NOT** shift all remaining elements to the left. 
+    """
+    def  __init__(self, max_size):
+        super().__init__(max_size)
+        self.type = "Isaac's Linear"
+
+
+    def dequeue(self) -> Union[str, None]:
+        '''Remove an item from the front of the queue'''
+        if self.is_empty():
+            return None
+
+        dequeued_item = self.queue[self.front]
+        self.front += 1
+        return dequeued_item
+
+class CircularQueue(Queue):
+    '''
+    Create a circular queue that utilizes empty spaces at the front of the array caused by removing elements from the queue.
+
+    Attributes:
+    - type (str): The type of the queue, set to "Circular".
+    - max_size (int): The maximum capacity of the circular queue.
+    - queue (list): The list representing the circular queue.
+    - rear (int): The index pointing to the last element in the queue.
+    - front (int): The index pointing to the front element in the queue. Set to -1 when the queue is empty.
+
+    Methods:
+    - display_queue(self): Displays the current status of the circular queue.
+    - is_full(self) -> bool: Returns True if the queue is full; otherwise, returns False.
+    - is_empty(self) -> bool: Returns True if the queue is empty; otherwise, returns False.
+    - enqueue(self, item_to_add) -> bool: Adds an item to the end of the circular queue. Returns True on success, False if the queue is full.
+    - dequeue(self): Removes and returns an item from the circular queue. Returns None if the queue is empty.
+
+    Note:
+    This implementation takes advantage of circular indexing to efficiently utilize the available space in the array.
+    When the queue is full, further enqueue operations are rejected.
+    Dequeue operations update the front and rear pointers, and if the queue becomes empty, these pointers are reset to -1.
+    '''
+    def __init__(self, max_size):
+        super().__init__(max_size)
+        self.type = "Circular"
+        self.queue = [None for x in range(max_size)]
+        self.rear = -1
+        self.front = -1
+
+    def is_full(self):
+        '''Return True if queue is full else False'''
+        return (self.rear + 1) % self.max_size == self.front
+
+    def is_empty(self):
+        '''Return True if the queue is empty, else return False'''
+        return self.front == -1
+
+    def enqueue(self, item_to_add):
+        '''Add an item to the end of the queue'''
+        if self.is_full():
+            return False
+        self.rear = (self.rear + 1) % self.max_size
+        self.queue[self.rear] = item_to_add
+        self.front = 0 if self.front == -1 else self.front
+        return True
+
+    def dequeue(self):
+        '''Remove an item from the queue'''
+        if self.is_empty():
+            return None
+        dequeued_item = self.queue[self.front]
+        if self.front == self.rear:
+            self.front = -1
+            self.rear = -1
+        else:
+            self.front = (self.front+1) % self.max_size
+        return dequeued_item
---
+++ src/test_queues.py
@@ -0,0 +1,103 @@
+"""
+Queue System
+
+This program allows users to interact with different types of queues, including Linear Queue, Isaac's Linear Queue, and Circular Queue.
+
+Author: Viren070 on GitHub
+"""
+
+import queues
+
+
+def get_integer_within_limits(text, lower_limit=None, upper_limit=None):
+    '''
+    Obtain an integer input within optional limits.
+
+    Parameters:
+    - text (str): The prompt to display to the user.
+    - lower_limit (int): The optional lower limit for the integer input.
+    - upper_limit (int): The optional upper limit for the integer input.
+
+    Returns:
+    int: The validated integer input within the specified limits.
+    '''
+    integer = None
+    while integer is None:
+        try:
+            integer = int(input(text))
+            if lower_limit is not None and integer < lower_limit:
+                print(f"\nYou must enter a number greater than {lower_limit}")
+                integer = None
+            elif upper_limit is not None and integer > upper_limit:
+                print(f"\nYou must enter a number lower than {upper_limit}")
+                integer = None
+        except ValueError:
+            print("\nPlease enter a valid integer")
+    return integer
+
+def initialise_queue():
+    '''
+    Initialize a queue based on user input for the maximum size and type of queue.
+
+    Returns:
+    LinearQueue or IsaacsLinearQueue or CircularQueue: The initialized queue based on user input.
+    '''
+    queue = None
+    max_queue_size = get_integer_within_limits("\nEnter the maximum size of the queue: > ", 0)
+    while queue is None:
+        queue_option = input("\nWhat queue do you wish to use? \n1 - Linear Queue\n2 - Circular Queue\n3 - Isaac Linear Queue\n: > ")
+
+        if queue_option == "1":
+            queue = queues.LinearQueue(max_queue_size)
+        elif queue_option == "2":
+            queue = queues.CircularQueue(max_queue_size)
+        elif queue_option == "3":
+            queue = queues.IsaacsLinearQueue(max_queue_size)
+        else:
+            print("\nInvalid Choice.\n")
+    return queue
+
+def run_queue_menu(queue):
+    '''
+    Run the menu for interacting with the given queue.
+
+    Parameters:
+    - queue (LinearQueue or IsaacsLinearQueue or CircularQueue): The queue to interact with.
+    '''
+    while queue is not None:
+        # menu for dequeuing and enqueuing
+        queue.display_queue()
+        user_choice = input("\nQueue Options:\n1 - Enqueue\n2 - Dequeue\n3 - Back to Main Menu\n: > ")
+
+        if user_choice == "1":  # handle enqueue
+            data = input("\nWhat do you want to enqueue? > ")
+            if queue.enqueue(data):
+                print(f"\n{data} was added to the queue")
+            else:
+                print(f"\nThe queue is full. {data} was not added to the queue")
+
+        elif user_choice == "2":  # handle dequeue
+            dequeue_result = queue.dequeue()
+            if dequeue_result is None:
+                print("\nThere was nothing to dequeue; the queue is empty.")
+            else:
+                print(f"\n{dequeue_result} was removed from the queue")
+
+        elif user_choice == "3":  # handle exit
+            queue = None
+        else:  # handle other inputs
+            print("\nInvalid choice")
+        if user_choice != "3":  # don't show this message if the user wants to exit
+            input("\nPress enter to continue....")
+
+if __name__ == "__main__":
+    print("Welcome to the Queue System. You can choose from 2 queueing methods. Namely: Linear and Circular.")
+    print("Isaac's Linear Queue will not shift all items to the left in the case of a dequeue.")
+    while True:
+        menu_choice = input("\nMain Menu:\n1 - Start\n2 - Exit\n: > ")
+        if menu_choice == "2":
+            break
+        if menu_choice != "1":
+            print("\nInvalid Choice.\n")
+        else:
+            run_queue_menu(initialise_queue())
